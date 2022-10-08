b3e38b5918cd263e624822efb69189829e661f04
Daddy <sergoffan@mail.ru>
2022-10-09 00:25:41 +0300

    repair

--- 01/E002_HelloUser/Program.cs
+++ 01/E002_HelloUser/Program.cs
@@ -1,5 +1,5 @@
 ﻿Console.WriteLine("Введите ваше имя ");
-string username = Console.ReadLine();
+string username = Console.ReadLine() ?? "0";
 Console.WriteLine("Привет, ");
 Console.WriteLine(username);
 
