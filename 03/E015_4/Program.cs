6b160a448d652e983d0be7153af49ef8e4d7b36f
Daddy <sergoffan@mail.ru>
2022-10-12 00:57:03 +0300

    add E015

---
+++ 03/E015_4/Program.cs
@@ -0,0 +1,18 @@
+﻿// Вид 4
+
+string Method4(int count, string text)
+{
+    int i = 0;
+    string result = String.Empty;
+
+    while (i < count)
+    {
+        result = result + text;
+        i++;
+    }
+    return result;
+}
+
+string res = Method4(10, "qwerty");
+Console.WriteLine(res);
+
